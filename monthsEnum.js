9115145de792ac0d4c31d89af27d79f12d620323
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 23:21:39 -0300

    add months enum

---
+++ monthsEnum.js
@@ -0,0 +1,16 @@
+const months = {
+  JAN: 'Jan',
+  FEV: 'Feb',
+  MAR: 'Mar',
+  ABR: 'Apr',
+  MAI: 'May',
+  JUN: 'Jun',
+  JUL: 'Jul',
+  AGO: 'Aug',
+  SET: 'Sep',
+  OUT: 'Oct',
+  NOV: 'Nov',
+  DEZ: 'Dec',
+}
+
+module.exports = months
