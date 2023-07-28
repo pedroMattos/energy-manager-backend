fc79ce3b47f0986d341fe2fe7f457cd633de2b71
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-07-27 23:23:12 -0300

    hide data on .env

--- pgConfig/index.js
+++ pgConfig/index.js
@@ -1,26 +1,14 @@
-// const pg = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     connectionString: config.DATABASE_URL,
-//     host: config["DB_HOST"],
-//     port: config["DB_PORT"],
-//     user: config["DB_USER"],
-//     database: config["DB_NAME"],
-//     password: config["DB_PASSWORD"],
-//     ssl: config["DB_SSL"] ? { rejectUnauthorized: false } : false,
-//   }
-// });
-
+require("dotenv").config();
 const knex = require('knex')({
   client: 'postgres',
   connection: async () => {
 
     return {
-      host : 'containers-us-west-116.railway.app',
-      port : 7044,
-      user : 'postgres',
-      password : 'yxWGXRSIBR3AKVOXhFvN',
-      database : 'railway',
+      host: process.env.HOST,
+      port: 5432,
+      user: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
     };
   }
 });
